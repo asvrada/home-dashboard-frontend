ef75f40e63b39b78d016b39d85766d2ee134ecf6
Zijie Wu <noojeff@gmail.com>
2020-04-25 21:24:24 -0700

    Small UI fix

--- src/components/views/ListView/Category.js
+++ src/components/views/ListView/Category.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 function Category({ node }) {
+  let componentCategory = null;
+  if (node.category !== null) {
+    componentCategory = (
+      <span>
+        <img className="category-icon"
+             src={process.env.PUBLIC_URL + "/static/test_icon.jpeg"}
+             alt="icon for category"/>
+        <div className="category-name">
+          {node.category.name}
+        </div>
+      </span>
+    );
+  }
+
   return (
     <div className="col-3 p-0 text-center word-break">
-      <img className="category-icon"
-           src={process.env.PUBLIC_URL + "/static/test_icon.jpeg"}
-           alt="icon for category"/>
-      <div className="category-name">
-        {node.category.name}
-      </div>
+      {componentCategory}
     </div>
   );
 }
--- src/components/views/ListView/DateBox.js
+++ src/components/views/ListView/DateBox.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+function isCurrentYear(year) {
+  const currentYear = (new Date()).getFullYear().toString();
+
+  return year.toString() === currentYear;
+}
+
 function DateBox({ date }) {
   const month = date[1].toString().padStart(2, "0");
   const day = date[2].toString().padStart(2, "0");
 
+  const componentYear = isCurrentYear(date[0])
+    ? null
+    : <span>{date[0]}-</span>;
+
   return (
     <div className="d-inline-flex m-1 p-1 date-box">
-      {date[0]}-{month}-{day}
+      <span>{componentYear}{month}-{day}</span>
     </div>
   );
 }
