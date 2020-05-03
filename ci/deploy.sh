a9ba0047cbc8742c8e9745e1eaa8069e23cc6ecc
Zijie Wu <noojeff@gmail.com>
2020-05-02 23:10:20 -0700

    Added deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,1 +1,15 @@
-echo "Deploy.sh run"
+#!/bin/sh
+set -e # Stop script from running if there are any errors
+
+IMAGE="hullcritical/dashboard-frontend"
+GIT_VERSION=$(git describe --always --abbrev --tags --long)
+
+# Build and tag image
+docker build -t ${IMAGE}:${GIT_VERSION} .
+docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
+
+# Log in to Docker Hub and push
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${IMAGE}:${GIT_VERSION}
+
+echo "Docker image pushed to Hub"
