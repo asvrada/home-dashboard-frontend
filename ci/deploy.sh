74fb35f374080ac3dbe59bbafeb23164a920e234
Zijie Wu <noojeff@gmail.com>
2021-08-01 10:56:52 -0700

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+
 echo "Running deploy.sh"
+
+# Call Servicebus
+SERVICEBUS_ENDPOINT="https://jeff-servicebusclient.azurewebsites.net"
+
+curl -d "{\"repo\":\"home-dashboard-frontend\"}" -H 'Content-Type: application/json' -X POST $SERVICEBUS_ENDPOINT
+
+echo "Finished deploy.sh"
